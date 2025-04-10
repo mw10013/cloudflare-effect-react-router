bac8d661799d88a424d48a4444d129b4b8a2adc6
Michael Wu <mw10013@gmail.com>
2025-04-10 16:00:12 -0400

    clean

--- functions/r1/app/routes/home.tsx
+++ functions/r1/app/routes/home.tsx
@@ -1,17 +1,14 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import { Welcome } from '../welcome/welcome'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: 'r1' }, { name: 'description', content: 'Welcome to r1' }]
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: `ENVIRONMENT: ${context.cloudflare.env.ENVIRONMENT}` };
+  return { message: `ENVIRONMENT: ${context.cloudflare.env.ENVIRONMENT}` }
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <div className="p-6">{loaderData.message}</div>
 }
