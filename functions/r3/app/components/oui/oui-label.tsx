d2be1053c972da20e9625edb7bf87a9be4d16e8a
Michael Wu <mw10013@gmail.com>
2025-04-13 13:52:13 -0400

    radio

--- functions/r3/app/components/oui/oui-label.tsx
+++ functions/r3/app/components/oui/oui-label.tsx
@@ -5,11 +5,13 @@ import { composeTailwindRenderProps } from './oui-base'
 // shadcn label: flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50
 // shandcn FormLabel: data-[error=true]:text-destructive
 // shadcn SelectLabel: text-muted-foreground px-2 py-1.5 text-xs
+// shadcn RadioGroupItem: border-input text-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
 export const labelStyles = tv({
   base: 'flex select-none items-center gap-2 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50',
-  // Variants are for rac components that structure with a <label> and have render props. Eg. checkbox and switch.
+  // Variants are for rac components that structure with a <label> and have render props. Eg. radio, checkbox and switch.
   variants: {
     isDisabled: {
+      // TODO: labelStyles: cursor-not-allowed for disabled?
       true: 'pointer-events-none opacity-50'
     }
   }
--- functions/r3/app/components/oui/oui-radio-group.tsx
+++ functions/r3/app/components/oui/oui-radio-group.tsx
@@ -1,5 +1,5 @@
 import type React from 'react'
-import { Circle } from 'lucide-react'
+import { Circle, CircleIcon } from 'lucide-react'
 import * as Rac from 'react-aria-components'
 import { tv } from 'tailwind-variants'
 import { baseStyles, composeTailwindRenderProps } from './oui-base'
@@ -15,22 +15,28 @@ export function RadioGroup(props: Rac.RadioGroupProps) {
   )
 }
 
-// TODO: radioStyles: isInvalid: text-foreground?
+// shadcn RadioGroupItem: border-input text-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
+// Radix has RadioGroupPrimitive.Item which is separate from label while RAC structures with a label.
 export const radioStyles = tv({
   extend: labelStyles,
-  base: 'flex items-center gap-2'
+  base: 'group flex items-center gap-2'
 })
 
+// // shadcn RadioGroupItem: border-input text-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
 export const radioIndicatorStyles = tv({
-  extend: baseStyles,
-  base: 'border-primary text-primary aspect-square size-4 rounded-full border shadow',
-  variants: {
-    isDisabled: {
-      // shadcn uses disabled:opacity-50. Use opacity-[0.714] since root (labelStyles) uses opacity-70
-      true: 'opacity-[0.714]'
-    }
-  }
+  base: 'border-input text-primary group-focus-visible:border-ring group-focus-visible:ring-ring/50 group-aria-invalid:ring-destructive/20 dark:group-aria-invalid:ring-destructive/40 group-aria-invalid:border-destructive dark:bg-input/30 shadow-xs aspect-square size-4 shrink-0 rounded-full border outline-none transition-[color,box-shadow] group-focus-visible:ring-[3px] group-disabled:cursor-not-allowed group-disabled:opacity-50'
 })
+
+// export const radioIndicatorStyles = tv({
+//   extend: baseStyles,
+//   base: 'border-primary text-primary aspect-square size-4 rounded-full border shadow',
+//   variants: {
+//     isDisabled: {
+//       // shadcn uses disabled:opacity-50. Use opacity-[0.714] since root (labelStyles) uses opacity-70
+//       true: 'opacity-[0.714]'
+//     }
+//   }
+// })
 
 export const radioCircleStyles = 'size-3.5 fill-primary'
 
@@ -38,10 +44,9 @@ export interface RadioProps extends Omit<Rac.RadioProps, 'children'> {
   children?: React.ReactNode
 }
 
-// shadcn: border-input text-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50
-
 export const Radio = ({ className, children, ...props }: RadioProps) => {
   return (
+    // Structure with a lablel.
     <Rac.Radio
       data-slot="radio-group-item"
       className={Rac.composeRenderProps(className, (className, renderProps) => radioStyles({ ...renderProps, className }))}
@@ -49,8 +54,9 @@ export const Radio = ({ className, children, ...props }: RadioProps) => {
     >
       {(renderProps) => (
         <>
-          <div className={radioIndicatorStyles({ ...renderProps })}>
-            {renderProps.isSelected && <Circle className={radioCircleStyles} />}
+          <div data-slot="radio-group-indicator" className={radioIndicatorStyles({ ...renderProps })}>
+            {/* {renderProps.isSelected && <Circle className={radioCircleStyles} />} */}
+            {renderProps.isSelected && <CircleIcon className="fill-primary absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2" />}
           </div>
           {children}
         </>
