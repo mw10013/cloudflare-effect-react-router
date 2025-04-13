aaa4e69a64a66a63771018082495da2706a328c2
Michael Wu <mw10013@gmail.com>
2025-04-12 22:01:34 -0400

    fix trigger

--- functions/r3/app/components/oui/oui-popover.tsx
+++ functions/r3/app/components/oui/oui-popover.tsx
@@ -9,45 +9,34 @@ import { tv } from 'tailwind-variants'
 // TODO: popoverStyles: SubmenuTrigger
 // TODO: popoverStyles: outline-none
 export const popoverStyles = tv({
-  base: 'rounded-md border bg-popover text-popover-foreground shadow-md data-[entering]:animate-in data-[exiting]:animate-out data-[entering]:fade-in-0 data-[exiting]:fade-out-0 data-[entering]:zoom-in-95 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2',
+  base: 'bg-popover text-popover-foreground data-[entering]:animate-in data-[exiting]:animate-out data-[entering]:fade-in-0 data-[exiting]:fade-out-0 data-[entering]:zoom-in-95 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2 rounded-md border shadow-md',
   variants: {
     trigger: {
       DialogTrigger: 'min-w-72 p-4',
       MenuTrigger: 'min-w-[8rem] overflow-hidden p-1',
       SubmenuTrigger: '',
-      Select: 'min-w-[--trigger-width]',
-      ComboBox: 'min-w-[--trigger-width]',
-    },
-  },
+      Select: 'min-w-(--trigger-width)',
+      ComboBox: 'min-w-(--trigger-width)'
+    }
+  }
 })
 
 type PopoverStylesTriggerKey = keyof typeof popoverStyles.variants.trigger
 
-function isPopoverStylesTriggerKey(
-  value: unknown
-): value is PopoverStylesTriggerKey {
-  return (
-    typeof value === 'string' &&
-    Object.keys(popoverStyles.variants.trigger).includes(value)
-  )
+function isPopoverStylesTriggerKey(value: unknown): value is PopoverStylesTriggerKey {
+  return typeof value === 'string' && Object.keys(popoverStyles.variants.trigger).includes(value)
 }
 
 // TODO: Popover menu: align center
-export const Popover = ({
-  className,
-  offset = 4,
-  ...props
-}: Rac.PopoverProps) => (
+export const Popover = ({ className, offset = 4, ...props }: Rac.PopoverProps) => (
   <Rac.Popover
     offset={offset}
-    className={Rac.composeRenderProps(
-      className,
-      (className, { trigger, ...renderProps }) =>
-        popoverStyles({
-          ...renderProps,
-          trigger: isPopoverStylesTriggerKey(trigger) ? trigger : undefined,
-          className,
-        })
+    className={Rac.composeRenderProps(className, (className, { trigger, ...renderProps }) =>
+      popoverStyles({
+        ...renderProps,
+        trigger: isPopoverStylesTriggerKey(trigger) ? trigger : undefined,
+        className
+      })
     )}
     {...props}
   />
