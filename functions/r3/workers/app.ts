8f22d48f5c427d1aa01ba5863495c7bc670c2aa0
Michael Wu <mw10013@gmail.com>
2025-04-16 12:43:24 -0400

    runtime

--- functions/r3/workers/app.ts
+++ functions/r3/workers/app.ts
@@ -1,23 +1,41 @@
-import { createRequestHandler } from "react-router";
+import { Effect, Layer, ManagedRuntime } from 'effect'
+import { createRequestHandler } from 'react-router'
+import * as CloudflareEx from '~/lib/CloudflareEx'
 
-declare module "react-router" {
+declare module 'react-router' {
   export interface AppLoadContext {
     cloudflare: {
-      env: Env;
-      ctx: ExecutionContext;
-    };
+      env: Env
+      ctx: ExecutionContext
+    }
+    runtime: ReturnType<typeof makeRuntime>
   }
 }
 
-const requestHandler = createRequestHandler(
-  () => import("virtual:react-router/server-build"),
-  import.meta.env.MODE
-);
+const requestHandler = createRequestHandler(() => import('virtual:react-router/server-build'), import.meta.env.MODE)
+
+export class SandboxService extends Effect.Service<SandboxService>()('SandboxService', {
+  accessors: true,
+  effect: Effect.gen(function* () {
+    return {
+      foo: () =>
+        Effect.gen(function* () {
+          yield* Effect.log('SandboxService: foo')
+          return 'bar'
+        })
+    }
+  })
+}) {}
+
+export const makeRuntime = () => {
+  return Layer.mergeAll(SandboxService.Default).pipe(CloudflareEx.provideLoggerAndConfig, ManagedRuntime.make)
+}
 
 export default {
   async fetch(request, env, ctx) {
     return requestHandler(request, {
       cloudflare: { env, ctx },
-    });
-  },
-} satisfies ExportedHandler<Env>;
+      runtime: makeRuntime()
+    })
+  }
+} satisfies ExportedHandler<Env>
