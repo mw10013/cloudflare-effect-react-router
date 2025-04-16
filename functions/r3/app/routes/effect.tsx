764615d47888c8f3da9006ab3e51c328ab3e0761
Michael Wu <mw10013@gmail.com>
2025-04-16 18:27:40 -0400

    cleanup

--- functions/r3/app/routes/effect.tsx
+++ functions/r3/app/routes/effect.tsx
@@ -1,19 +1,11 @@
-import type { ManagedRuntime } from 'effect'
-import type { AppLoadContext } from 'react-router'
 import type { Route } from './+types/effect'
 import { Effect } from 'effect'
-
-export const loaderFunction =
-  <A, E, P extends { context: AppLoadContext }>(
-    f: (props: P) => Effect.Effect<A, E, ManagedRuntime.ManagedRuntime.Context<AppLoadContext['runtime']>>
-  ) =>
-  (props: P) =>
-    f(props).pipe(props.context.runtime.runPromise)
+import { loaderFunction, SandboxService } from 'workers/app'
 
 export const loader = loaderFunction(({ context }: Route.LoaderArgs) =>
   Effect.gen(function* () {
     yield* Effect.log('loader')
-    return { message: `ENVIRONMENT: ${context.cloudflare.env.ENVIRONMENT}` }
+    return { message: `ENVIRONMENT: ${context.cloudflare.env.ENVIRONMENT}`, foo: yield* SandboxService.foo() }
   })
 )
 
@@ -24,38 +16,3 @@ export default function RouteComponent(props: Route.ComponentProps) {
     </div>
   )
 }
-
-/*
-
-export const handler =
-  <A, E>(
-    h: (
-      ...args: Parameters<Handler<AppEnv>>
-    ) => Effect.Effect<A | Promise<A>, E, ManagedRuntime.ManagedRuntime.Context<Parameters<Handler<AppEnv>>[0]['var']['runtime']>>
-  ) =>
-  (...args: Parameters<Handler<AppEnv>>) =>
-    h(...args).pipe(
-      Effect.flatMap((response) => (Predicate.isPromise(response) ? Effect.tryPromise(() => response) : Effect.succeed(response))),
-      orErrorResponse(args[0]),
-      args[0].var.runtime.runPromise
-    )
-
-
-export const makeRemixRuntime = <R, E>(layer: Layer.Layer<R, E, never>) => {
-  const runtime = ManagedRuntime.make(layer);
-
-  const loaderFunction =
-    <A, E>(
-      body: (...args: Parameters<LoaderFunction>) => Effect.Effect<A, E, R>
-    ): {
-      (...args: Parameters<LoaderFunction>): Promise<A>;
-    } =>
-    (...args) =>
-      runtime.runPromise(body(...args));
-
-  return { loaderFunction };
-};
-
-export const loader = loaderFunction(() => TodoRepo.getAllTodos);
-
-*/
--- functions/r3/workers/app.ts
+++ functions/r3/workers/app.ts
@@ -1,3 +1,4 @@
+import type { AppLoadContext } from 'react-router'
 import { Effect, Layer, ManagedRuntime } from 'effect'
 import { createRequestHandler } from 'react-router'
 import * as CloudflareEx from '~/lib/CloudflareEx'
@@ -27,7 +28,14 @@ export class SandboxService extends Effect.Service<SandboxService>()('SandboxSer
   })
 }) {}
 
-export const makeRuntime = () => {
+export const loaderFunction =
+  <A, E, P extends { context: AppLoadContext }>(
+    f: (props: P) => Effect.Effect<A, E, ManagedRuntime.ManagedRuntime.Context<AppLoadContext['runtime']>>
+  ) =>
+  (props: P) =>
+    f(props).pipe(props.context.runtime.runPromise)
+
+const makeRuntime = () => {
   return Layer.mergeAll(SandboxService.Default).pipe(CloudflareEx.provideLoggerAndConfig, ManagedRuntime.make)
 }
 
@@ -39,3 +47,39 @@ export default {
     })
   }
 } satisfies ExportedHandler<Env>
+
+/*
+
+export const handler =
+  <A, E>(
+    h: (
+      ...args: Parameters<Handler<AppEnv>>
+    ) => Effect.Effect<A | Promise<A>, E, ManagedRuntime.ManagedRuntime.Context<Parameters<Handler<AppEnv>>[0]['var']['runtime']>>
+  ) =>
+  (...args: Parameters<Handler<AppEnv>>) =>
+    h(...args).pipe(
+      Effect.flatMap((response) => (Predicate.isPromise(response) ? Effect.tryPromise(() => response) : Effect.succeed(response))),
+      orErrorResponse(args[0]),
+      args[0].var.runtime.runPromise
+    )
+
+
+export const makeRemixRuntime = <R, E>(layer: Layer.Layer<R, E, never>) => {
+  const runtime = ManagedRuntime.make(layer);
+
+  const loaderFunction =
+    <A, E>(
+      body: (...args: Parameters<LoaderFunction>) => Effect.Effect<A, E, R>
+    ): {
+      (...args: Parameters<LoaderFunction>): Promise<A>;
+    } =>
+    (...args) =>
+      runtime.runPromise(body(...args));
+
+  return { loaderFunction };
+};
+
+export const loader = loaderFunction(() => TodoRepo.getAllTodos);
+
+*/
+
