af239ab4ae9b372b0097ad4efc7d74b943de0758
Michael Wu <mw10013@gmail.com>
2025-04-26 19:51:08 -0400

    subjects

--- functions/s/workers/app.ts
+++ functions/s/workers/app.ts
@@ -1,6 +1,7 @@
 import * as Hono from 'hono'
 import { createRequestHandler } from 'react-router'
 import { appLoadContext, makeRuntime } from '../app/lib/ReactRouter'
+import { createOpenAuth } from './openauth'
 
 declare module 'react-router' {
   export interface AppLoadContext {
@@ -12,39 +13,20 @@ declare module 'react-router' {
   }
 }
 
-// const requestHandler = createRequestHandler(() => import('virtual:react-router/server-build'), import.meta.env.MODE)
-
 export default {
   async fetch(request, env, ctx) {
     const hono = new Hono.Hono()
     const runtime = makeRuntime(env)
-    // ctx.waitUntil(runtime.dispose())
-    // return requestHandler(request, initialContext)
+    const openAuth = createOpenAuth({ env, runtime })
+    hono.route('/', openAuth)
     hono.all('*', (c) => {
       const requestHandler = createRequestHandler(() => import('virtual:react-router/server-build'), import.meta.env.MODE)
       const initialContext = new Map([[appLoadContext, { cloudflare: { env, ctx }, runtime }]])
       return requestHandler(c.req.raw, initialContext)
     })
-    return hono.fetch(request, env, ctx)
+    const response = await hono.fetch(request, env, ctx)
+    ctx.waitUntil(runtime.dispose())
+    return response
   },
   async queue() {}
 } satisfies ExportedHandler<Env>
-
-/*
-
-const app = new Hono();
-
-// Add more routes here
-
-app.get("*", c => {
-  const requestHandler = createRequestHandler(
-    () => import("virtual:react-router/server-build"),
-    import.meta.env.MODE
-  );
-
-  return requestHandler(c.req.raw, {
-    cloudflare: { env: c.env, ctx: c.executionCtx }
-  })
-})
-
-*/
