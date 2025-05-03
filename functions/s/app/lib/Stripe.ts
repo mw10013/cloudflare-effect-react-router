224c2f55c3d2bdfbbd6df526a221fd5f1e5f7217
Michael Wu <mw10013@gmail.com>
2025-05-03 01:53:28 -0400

    stripe

--- functions/s/app/lib/Stripe.ts
+++ functions/s/app/lib/Stripe.ts
@@ -1,22 +1,8 @@
 import type { Stripe as StripeType } from 'stripe'
-import { Cloudflare, ConfigEx, ErrorEx } from '@workspace/shared'
-// import { DurableObject } from 'cloudflare:workers'
-import { Cause, Config, ConfigError, Effect, Either, Layer, Logger, LogLevel, ManagedRuntime, Option, Predicate, Redacted } from 'effect'
+import { ConfigEx, ErrorEx } from '@workspace/shared'
+import { Cause, Config, ConfigError, Effect, Either, Option, Predicate, Redacted } from 'effect'
 import { Stripe as StripeClass } from 'stripe'
 import { IdentityMgr } from './IdentityMgr'
-
-/*
-
-3:28:05 PM [vite] Internal server error: Failed to resolve import "cloudflare:workers" from "app/lib/Stripe.ts". Does the file exist?
-  Plugin: vite:import-analysis
-  File: /Users/mw/Documents/src/cloudflare-effect-react-router/functions/s/app/lib/Stripe.ts:3:30
-  1  |  import { Cloudflare, ConfigEx, ErrorEx } from "@workspace/shared";
-  2  |  import { DurableObject } from "cloudflare:workers";
-     |                                 ^
-  3  |  import { Cause, Config, ConfigError, Effect, Either, Layer, Logger, LogLevel, ManagedRuntime, Option, Predicate, Redacted } from "effect";
-  4  |  import { Stripe as StripeClass } from "stripe";
-
-*/
 
 export class Stripe extends Effect.Service<Stripe>()('Stripe', {
   accessors: true,
@@ -388,70 +374,3 @@ export class Stripe extends Effect.Service<Stripe>()('Stripe', {
     }
   })
 }) {}
-
-// const makeRuntime = (env: Env) => {
-//   return Layer.mergeAll(Stripe.Default).pipe(Cloudflare.provideLoggerAndConfig(env), ManagedRuntime.make)
-// }
-
-// export class StripeDurableObject extends DurableObject<Env> {
-//   storage: DurableObjectStorage
-//   sql: SqlStorage
-//   runtime: ReturnType<typeof makeRuntime>
-
-//   constructor(ctx: DurableObjectState, env: Env) {
-//     super(ctx, env)
-//     this.storage = ctx.storage
-//     this.sql = ctx.storage.sql
-//     this.runtime = makeRuntime(env)
-
-//     this.sql.exec(`create table if not exists events(
-// 			customerId text primary key,
-// 			count integer not null default 1,
-// 			createdAt integer default (unixepoch()));
-// 			create index if not exists idxEventsCreatedAt on events(createdAt);`)
-//   }
-
-//   async handleEvent(customerId: string) {
-//     await Effect.gen(this, function* () {
-//       const alarm = yield* Effect.tryPromise(() => this.storage.getAlarm())
-//       if (alarm === null) {
-//         const STRIPE_SYNC_INTERVAL_SEC = yield* Config.integer('STRIPE_SYNC_INTERVAL_SEC')
-//         yield* Effect.tryPromise(() => this.storage.setAlarm(Date.now() + 1000 * STRIPE_SYNC_INTERVAL_SEC))
-//       }
-//       const { count } = this.sql
-//         .exec<{
-//           count: number
-//         }>(
-//           `insert into events (customerId) values (?) on conflict (customerId) do update set count = count + 1 returning count`,
-//           customerId
-//         )
-//         .one()
-//       yield* Effect.log(`StripeDurableObject: handleEvent: customerId: ${customerId} count: ${count}`)
-//     }).pipe(this.runtime.runPromise)
-//   }
-
-//   async alarm() {
-//     await Effect.gen(this, function* () {
-//       const STRIPE_SYNC_BATCH_SIZE = yield* Config.integer('STRIPE_SYNC_BATCH_SIZE')
-//       const events = this.sql
-//         .exec<{ customerId: string; count: number }>(`select * from events order by createdAt asc limit ?`, STRIPE_SYNC_BATCH_SIZE + 1)
-//         .toArray()
-//       yield* Effect.log(
-//         `StripeDurableObject: alarm: eventCount: ${events.length} STRIPE_SYNC_BATCH_SIZE: ${STRIPE_SYNC_BATCH_SIZE}`,
-//         events
-//       )
-//       if (events.length > STRIPE_SYNC_BATCH_SIZE) {
-//         const STRIPE_SYNC_INTERVAL_SEC = yield* Config.integer('STRIPE_SYNC_INTERVAL_SEC')
-//         yield* Effect.tryPromise(() => this.storage.setAlarm(Date.now() + 1000 * STRIPE_SYNC_INTERVAL_SEC))
-//       }
-//       const effects = events.slice(0, STRIPE_SYNC_BATCH_SIZE).map(({ customerId, count }) =>
-//         Effect.gen(this, function* () {
-//           yield* Stripe.syncStripeData(customerId)
-//           yield* Effect.try(() => this.sql.exec(`delete from events where customerId = ?1 and count = ?2`, customerId, count))
-//           yield* Effect.log(`StripeDurableObject: alarm: syncStripeData customerId: ${customerId} count: ${count}`)
-//         })
-//       )
-//       yield* Effect.all(effects, { concurrency: 5 })
-//     }).pipe(this.runtime.runPromise)
-//   }
-// }
