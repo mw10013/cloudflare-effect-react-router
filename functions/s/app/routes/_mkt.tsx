9182242a731324ed9d1f49a211db6afe0665e5ca
Michael Wu <mw10013@gmail.com>
2025-05-05 16:58:43 -0400

    pricint

--- functions/s/app/routes/_mkt.tsx
+++ functions/s/app/routes/_mkt.tsx
@@ -7,9 +7,9 @@ import * as ReactRouter from '~/lib/ReactRouter'
 
 export const loader = ReactRouter.routeEffect(({ context }: Route.LoaderArgs) =>
   Effect.gen(function* () {
-    const alc = context.get(ReactRouter.appLoadContext)
-    yield* Effect.log({ message: '_mkt: loader', sessionUser: alc.session.get('sessionUser') })
-    return { sessionUser: alc.session.get('sessionUser') }
+    const appLoacContext = context.get(ReactRouter.appLoadContext)
+    yield* Effect.log({ message: '_mkt: loader', sessionUser: appLoacContext.session.get('sessionUser') })
+    return { sessionUser: appLoacContext.session.get('sessionUser') }
   })
 )
 
