e677a4bf8383f413a5dbb0431ff7f2daf968d195
Michael Wu <mw10013@gmail.com>
2025-05-09 17:34:21 -0400

    menu styles

--- functions/oui/src/oui-menu.tsx
+++ functions/oui/src/oui-menu.tsx
@@ -1,12 +1,26 @@
 import * as Rac from 'react-aria-components'
 import { tv } from 'tailwind-variants'
 
+/*
+#fetch https://react-spectrum.adobe.com/react-aria/Menu.html
+#fetch https://react-spectrum.adobe.com/react-aria/Popover.html
+#fetch https://ui.shadcn.com/docs/components/dropdown-menu
+#fetch https://react-spectrum.adobe.com/react-aria/collections.html
+#fetch https://react-spectrum.adobe.com/react-aria/ListBox.html
+*/
+
+// shadcn DropdownMenuContent: bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 max-h-(--radix-dropdown-menu-content-available-height) min-w-[8rem] origin-(--radix-dropdown-menu-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-md border p-1 shadow-md
 export const menu = tv({
-  base: ''
+  // No background, border, shadow, or animation classes here as they are on the Popover
+  base: 'max-h-[theme(spacing.72)] min-w-[8rem] overflow-y-auto overflow-x-hidden p-1'
 })
 
 export const Menu = <T extends object>({ className, ...props }: Rac.MenuProps<T>) => (
-  <Rac.Menu className={Rac.composeRenderProps(className, (className, renderProps) => menu({ ...renderProps, className }))} {...props} />
+  <Rac.Menu
+    data-slot="dropdown-menu-content"
+    className={Rac.composeRenderProps(className, (className, renderProps) => menu({ ...renderProps, className }))}
+    {...props}
+  />
 )
 
 /* shadcn DropdownMenuItem
