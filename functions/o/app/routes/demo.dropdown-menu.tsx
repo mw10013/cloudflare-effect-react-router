90d5bdfc5c01a2195fb016b7015b70bc0d12c694
Michael Wu <mw10013@gmail.com>
2025-05-09 19:11:47 -0400

    @source

--- functions/o/app/routes/demo.dropdown-menu.tsx
+++ functions/o/app/routes/demo.dropdown-menu.tsx
@@ -1,6 +1,13 @@
+import { tv } from 'tailwind-variants'
 import { DemoContainer } from '~/components/demo-container'
 import { DropdownMenuDemo } from '~/components/dropdown-menu-demo'
 import { OuiDropdownMenuDemo } from '~/components/oui-dropdown-menu-demo'
+
+export const menu = tv({
+  // No background, border, shadow, or animation classes here as they are on the Popover
+  base: 'max-h-[theme(spacing.72)] min-w-[8rem] overflow-y-auto overflow-x-hidden p-1'
+})
+
 
 export default function RouteComponent() {
   return (
