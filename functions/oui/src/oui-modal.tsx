fabfd23fdc41f3be56bc8554e2fa33c09388dd17
Michael Wu <mw10013@gmail.com>
2025-05-16 00:25:51 -0400

    Refactor oui-modal.tsx: streamline modalOverlayStyles and modalStyles by consolidating animation variants into base styles for improved readability and performance

--- functions/oui/src/oui-modal.tsx
+++ functions/oui/src/oui-modal.tsx
@@ -4,6 +4,7 @@ import { tv } from "tailwind-variants";
 /*
 #fetch https://react-spectrum.adobe.com/react-aria/Modal.html
 #fetch https://react-spectrum.adobe.com/react-aria/Dialog.html
+#fetch https://react-spectrum.adobe.com/react-aria/styling.html#tailwind-css-1
 #fetch https://ui.shadcn.com/docs/components/alert-dialog
 #fetch https://ui.shadcn.com/docs/components/dialog
 */
@@ -12,15 +13,7 @@ import { tv } from "tailwind-variants";
  * Derived from shadcn DialogOverlay
  */
 export const modalOverlayStyles = tv({
-  base: "fixed inset-0 z-50 bg-black/50",
-  variants: {
-    isEntering: {
-      true: "animate-in fade-in-0",
-    },
-    isExiting: {
-      true: "animate-out fade-out-0",
-    },
-  },
+  base: "data-[entering]:animate-in data-[entering]:fade-in-0 data-[exiting]:animate-out data-[exiting]:fade-out-0 fixed inset-0 z-50 bg-black/50",
 });
 
 export function ModalOverlay({ className, ...props }: Rac.ModalOverlayProps) {
@@ -38,15 +31,7 @@ export function ModalOverlay({ className, ...props }: Rac.ModalOverlayProps) {
  * Derived from shadcn DialogContent
  */
 export const modalStyles = tv({
-  base: "bg-background fixed left-[50%] top-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
-  variants: {
-    isEntering: {
-      true: "animate-in fade-in-0 zoom-in-95",
-    },
-    isExiting: {
-      true: "animate-out fade-out-0 zoom-out-95",
-    },
-  },
+  base: "bg-background data-[entering]:animate-in data-[entering]:fade-in-0 data-[entering]:zoom-in-95 data-[exiting]:animate-out data-[exiting]:fade-out-0 data-[exiting]:zoom-out-95 fixed left-[50%] top-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-200 sm:max-w-lg",
 });
 
 export function Modal({ className, ...rest }: Rac.ModalOverlayProps) {
