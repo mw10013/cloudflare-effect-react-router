e5600a808abaa31b26d00881bac0fbfd58c091d4
Michael Wu <mw10013@gmail.com>
2025-05-17 15:13:54 -0400

    Add OuiTableDemo component to display invoice data in a table format

--- functions/o/app/routes/demo.table.tsx
+++ functions/o/app/routes/demo.table.tsx
@@ -1,10 +1,12 @@
 import { DemoContainer } from "~/components/demo-container";
+import { OuiTableDemo } from "~/components/oui-table-demo";
 import { TableDemo } from "~/components/table-demo";
 
 export default function RouteComponent() {
   return (
     <DemoContainer className="flex-row">
       <TableDemo />
+      <OuiTableDemo />
     </DemoContainer>
   );
 }
