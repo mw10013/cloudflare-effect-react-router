54eaf2249d4c2aba3d2dbb7cc50d9efe09085945
Michael Wu <mw10013@gmail.com>
2025-05-18 12:36:20 -0400

    feat: integrate theme provider and router components, enhance styling with Tailwind CSS, and update dependencies

--- functions/i/app/root.tsx
+++ functions/i/app/root.tsx
@@ -1,3 +1,4 @@
+import type { Route } from "./+types/root";
 import {
   isRouteErrorResponse,
   Links,
@@ -5,10 +6,10 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigate,
 } from "react-router";
-
-import type { Route } from "./+types/root";
 import "./app.css";
+import { RouterProvider } from "react-aria-components";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +25,7 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
   return (
     <html lang="en">
       <head>
@@ -33,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <RouterProvider navigate={navigate}>{children}</RouterProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -62,11 +64,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="container mx-auto p-4 pt-16">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
--- functions/i/app/routes/_index.tsx
+++ functions/i/app/routes/_index.tsx
@@ -1,13 +1,13 @@
-import type { Route } from './+types/_index'
+import type { Route } from "./+types/_index";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: 'r1' }, { name: 'description', content: 'Welcome to r1' }]
+  return [{ title: "i" }, { name: "description", content: "Welcome to r1" }];
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: `ENVIRONMENT: ${context.cloudflare.env.ENVIRONMENT}` }
+  return { message: `ENVIRONMENT: ${context.cloudflare.env.ENVIRONMENT}` };
 }
 
 export default function RouteComponent({ loaderData }: Route.ComponentProps) {
-  return <div className="p-6">{loaderData.message}</div>
+  return <div className="p-6">{loaderData.message}</div>;
 }
