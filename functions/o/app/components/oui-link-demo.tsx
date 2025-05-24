50f5568335a72fbe9b55388ddfeb5510e74f7558
Michael Wu <mw10013@gmail.com>
2025-05-24 13:12:50 -0400

    feat: restructure OuiLinkDemo to include multiple login cards with improved layout and accessibility

--- functions/o/app/components/oui-link-demo.tsx
+++ functions/o/app/components/oui-link-demo.tsx
@@ -11,55 +11,103 @@ import {
 export function OuiLinkDemo() {
   return (
     <div className="flex flex-col items-start gap-4">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <Oui.TextFieldEx
-                label="Email"
-                placeholder="m@example.com"
-                isRequired
-              />
-              <Oui.TextFieldEx
-                name="password"
-                type="password"
-                isRequired
-                label={
-                  <div className="flex items-center">
-                    <Oui.Label>Password</Oui.Label>
-                    <a
-                      href="#"
-                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                }
-              />
+      <div className="flex w-full gap-4">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <Oui.TextFieldEx
+                  label="Email"
+                  placeholder="m@example.com"
+                  isRequired
+                />
+                <Oui.TextFieldEx
+                  name="password"
+                  type="password"
+                  isRequired
+                  label={
+                    <div className="flex items-center">
+                      <Oui.Label>Password</Oui.Label>
+                      <a
+                        href="#"
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    </div>
+                  }
+                />
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Oui.Button type="submit" className="w-full">
+              Login
+            </Oui.Button>
+            <Oui.Button variant="outline" className="w-full">
+              Login with Google
+            </Oui.Button>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
             </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Oui.Button type="submit" className="w-full">
-            Login
-          </Oui.Button>
-          <Oui.Button variant="outline" className="w-full">
-            Login with Google
-          </Oui.Button>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <a href="#" className="underline underline-offset-4">
-              Sign up
-            </a>
-          </div>
-        </CardFooter>
-      </Card>
+          </CardFooter>
+        </Card>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <Oui.TextFieldEx
+                  label="Email"
+                  placeholder="m@example.com"
+                  isRequired
+                />
+                <Oui.TextFieldEx
+                  name="password"
+                  type="password"
+                  isRequired
+                  label={
+                    <div className="flex items-center">
+                      <Oui.Label>Password</Oui.Label>
+                      <Oui.Link href="#" className="ml-auto inline-block">
+                        Forgot your password?
+                      </Oui.Link>
+                    </div>
+                  }
+                />
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Oui.Button type="submit" className="w-full">
+              Login
+            </Oui.Button>
+            <Oui.Button variant="outline" className="w-full">
+              Login with Google
+            </Oui.Button>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Oui.Link href="#" className="underline">
+                Sign up
+              </Oui.Link>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
 
       <Oui.Link href="/play">Play</Oui.Link>
       <Oui.Link href="/play" className="underline">
